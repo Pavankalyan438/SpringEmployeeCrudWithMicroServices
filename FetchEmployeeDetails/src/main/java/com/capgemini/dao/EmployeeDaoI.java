518ae67a7bf0b33204d44b893f2744c12c96dd20
pavankalyan438 <pavankalyannaini438@gmail.com>
2020-04-16 12:20:35 +0530

    findOne

---
+++ FetchEmployeeDetails/src/main/java/com/capgemini/dao/EmployeeDaoI.java
@@ -0,0 +1,9 @@
+package com.capgemini.dao;
+
+import com.capgemini.entity.Employee;
+
+public interface EmployeeDaoI {
+
+	Employee fetchEmployee(int id);
+
+}
---
+++ FetchEmployeeDetails/src/main/java/com/capgemini/service/EmployeeService.java
@@ -0,0 +1,21 @@
+package com.capgemini.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.capgemini.dao.EmployeeDaoI;
+import com.capgemini.entity.Employee;
+
+@Service
+@Transactional
+public class EmployeeService implements EmployeeServiceI{
+	@Autowired
+	private EmployeeDaoI dao;
+
+	@Override
+	public Employee fetchEmployee(int id) {
+		return dao.fetchEmployee(id);
+	}
+}
