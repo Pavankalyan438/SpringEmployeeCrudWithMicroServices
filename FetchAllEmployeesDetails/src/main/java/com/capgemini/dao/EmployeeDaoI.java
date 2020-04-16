27535d3e476d96e557570c55f75287043ace6934
pavankalyan438 <pavankalyannaini438@gmail.com>
2020-04-16 12:18:11 +0530

    FetchAll

---
+++ FetchAllEmployeesDetails/src/main/java/com/capgemini/dao/EmployeeDaoI.java
@@ -0,0 +1,11 @@
+package com.capgemini.dao;
+
+import java.util.List;
+
+import com.capgemini.entity.Employee;
+
+public interface EmployeeDaoI {
+
+	List<Employee> getAllEmployees();
+
+}
---
+++ FetchAllEmployeesDetails/src/main/java/com/capgemini/service/EmployeeServiceI.java
@@ -0,0 +1,11 @@
+package com.capgemini.service;
+
+import java.util.List;
+
+import com.capgemini.entity.Employee;
+
+public interface EmployeeServiceI {
+
+	List<Employee> getAllEmployees();
+
+}
